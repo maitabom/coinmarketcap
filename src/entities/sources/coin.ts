29e8dcf71e1538ec7cb063b9ccbfd5c03230bd13
Fábio Valentim <maitabom@gmail.com>
2024-05-12 17:42:34 -0300

    Criação de entidade para código API

---
+++ src/entities/sources/coin.ts
@@ -0,0 +1,17 @@
+interface CoinSource {
+    id: string;
+    rank: string;
+    maxSupply: string;
+    marketCapUsd: string;
+    name: string;
+    symbol: string;
+    priceUsd: string;
+    vwap24Hr: string;
+    volumeUsd24Hr: string;
+    explorer: string;
+    supply: string;
+    changePercent24Hr: string;
+  }
+  
+  export default CoinSource;
+  
